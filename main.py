ecbce534ca24dfd7ec76b3e1510965314e670e49
Ibar-Dev <ibarvivas@gmail.com>
2026-05-14 10:26:47 +0200

    Actualizar el título de la aplicación y agregar botón de acceso; crear archivos de inicialización para el módulo

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     # Creamos un control de texto con formato de título y lo agregamos a la página
-    titulo = ft.Text("Título de mi Aplicación", size=32, weight=ft.FontWeight.BOLD)
+
+    titulo = ft.Text("Bienvenid@ a este Onbording", size=32, weight=ft.FontWeight.BOLD)
     page.add(titulo)
+
+    boton_entrada = ft.Button(
+        "Acceder"
+    )
+    page.add(boton_entrada)
 
 ft.app(target=main)
     
